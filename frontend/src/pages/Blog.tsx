57ee2a0c65efa649012c5708119c9e61f47eb24d
Pruthvi Purohit <pruthvipurohit@gmail.com>
2024-05-06 06:33:44 +0530

    Fixing blog undefined assignment issue

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -5,7 +5,7 @@ export const Blog = () => {
 const { id } = useParams();
 const { loading, blog } = useBlog({ id : id || "" });
 
-if (loading) {
+if (loading || !blog) {
     return <div className="text-center h-screen flex items-center justify-center">
     <div role="status">
         <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
